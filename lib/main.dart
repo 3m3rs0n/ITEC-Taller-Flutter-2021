685c9656020d294e03b467b9cb69d375f058f57f
Alvaro Martinez <alvardud@gmail.com>
2021-10-02 10:44:01 -0400

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
@@ -9,23 +10,28 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Material App nos permite obtener una guia de estilos para nuestra aplicacion
     return MaterialApp(
+      //las propiedas de los widgets los separamos con una coma
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: MyWidget(),
+          child: CupertinoButton(
+              color: Colors.blue[900],
+              child: const Text("Soy un cupertino button",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                  )),
+              //Algunas propiedades requieren como parametro a una funcion
+              //y esa funcion en algunos casos sera una funcion anonima
+              onPressed: () {
+                //las funciones (logica) las finalizamos con punto y coma
+                print("Hola mundo");
+              }),
         ),
       ),
-    );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Hello, World!',
-      style: Theme.of(context).textTheme.headline4,
     );
   }
 }
